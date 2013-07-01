b173dda2dff383ea0de13fb4fa1208cdef929fd0
SimonEbner <Simon.Ebner@gmail.com>
2013-07-01 12:14:50 +0200

    Wait for the robot to be initialized

--- navigation_test_skeleton/src/navigation_test_skeleton/navigation_test.py
+++ navigation_test_skeleton/src/navigation_test_skeleton/navigation_test.py
@@ -5,7 +5,7 @@ import rospy, rostopic
 import os, logging, sys
 import cob_srvs.srv, time
 import unittest, rostest
-import std_srvs, std_srvs.srv
+import std_srvs, std_srvs.srv, std_msgs, threading
 from navigationStatusPublisher                import NavigationStatusPublisher
 from navigation_test_helper.positionResolver  import PositionResolver
 from navigation_test_helper.metricsObserverTF import MetricsObserverTF
@@ -13,9 +13,9 @@ from navigation_test_helper.tolerance         import Tolerance
 from navigation_test_helper.position          import Position
 from navigation_test_helper.navigator         import Navigator
 
+
 class TestNavigation( unittest.TestCase ):
     def setUp( self ):
-
         rospy.loginfo( 'Setting navigator' )
         self.navigator = Navigator( '/move_base' )
         self._metricsObserver = MetricsObserverTF()
@@ -27,8 +27,13 @@ class TestNavigation( unittest.TestCase ):
         self.tolerance = Tolerance( xy=0.2, theta=.3 )
         self.positionResolver = PositionResolver()
 
+        rospy.loginfo( 'Waiting for robot to be ready' )
+        robotReadyLock = self._waitForRobotToBeReady()
+        robotReadyLock.acquire()
+
         rospy.loginfo( 'Waiting for Bag Recorder' )
         self._stopBagRecording = self._waitForBagRecorder()
+
 
     def _getSetting( self ):
         return {
@@ -37,6 +42,17 @@ class TestNavigation( unittest.TestCase ):
             'navigation': rospy.get_param( '~navigation' ),
             'repository': rospy.get_param( '~repository' )
         }
+
+    def _waitForRobotToBeReady( self ):
+        self._robotReadyLock = threading.Lock()
+        subscriber = rospy.Subscriber( '/navigation_test/robot_ready', 
+                std_msgs.msg.String, self._callbackRobotReady )
+        self._robotReadyLock.acquire()
+        return self._robotReadyLock
+
+    def _callbackRobotReady( self, msg ):
+        rospy.loginfo( 'Robot ready. Releasing lock' )
+        self._robotReadyLock.release()
 
     def _waitForBagRecorder( self ):
         rospy.wait_for_service( '/logger/stop' )
@@ -87,6 +103,5 @@ class TestNavigation( unittest.TestCase ):
 
 if __name__ == '__main__':
     rospy.init_node( 'navigation_test_schalal' )
-    rospy.loginfo(" Doing some stuff" )
     rostest.rosrun( 'navgation_test', 'test_navigation',
         TestNavigation, sys.argv)
