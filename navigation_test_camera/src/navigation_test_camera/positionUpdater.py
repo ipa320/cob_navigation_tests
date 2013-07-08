aaa0af789c722c63301273781d96e5cc57ae96f9
SimonEbner <Simon.Ebner@gmail.com>
2013-07-08 18:39:09 +0200

    Rename z to zOffset

--- navigation_test_camera/src/navigation_test_camera/positionUpdater.py
+++ navigation_test_camera/src/navigation_test_camera/positionUpdater.py
@@ -10,18 +10,18 @@ from navigation_test_helper.positionResolver import PositionResolver
 from navigation_test_helper.gazeboHelper import ModelStateSetter
 
 class CameraUpdater( threading.Thread ):
-    def __init__( self, updateRate, z ):
+    def __init__( self, updateRate, zOffset ):
         threading.Thread.__init__( self )
         self._active     = True
         self._updateRate = updateRate
-        self._z          = z
+        self._zOffset    = zOffset
 
     def run( self ):
         positionResolver = PositionResolver()
         modelStateSetter = ModelStateSetter()
         while self._active and not rospy.is_shutdown():
             position = positionResolver.getPosition()
-            modelStateSetter.set( 'camera_model', position, z=self._z )
+            modelStateSetter.set( 'camera_model', position, z=self._zOffset )
             time.sleep( self._updateRate )
 
     def stop( self ):
@@ -29,9 +29,9 @@ class CameraUpdater( threading.Thread ):
 
 if __name__ == '__main__':
     rospy.init_node( 'camera_position_updater' )
-    z          = rospy.get_param( '~z' )
+    zOffset    = rospy.get_param( '~zOffset' )
     updateRate = float( rospy.get_param( '~updateRate' ))
-    updater    = CameraUpdater( updateRate, z )
+    updater    = CameraUpdater( updateRate, zOffset )
 
     updater.start()
     rospy.spin()
