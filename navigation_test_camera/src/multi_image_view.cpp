44d163ed8b8de2b85c05280a1acde9224c02e904
ipa-jsf <jsf@ipa.fhg.de>
2013-08-05 11:10:14 +0200

    Removed multi_image_view from CMakefile

--- navigation_test_camera/src/multi_image_view.cpp
+++ navigation_test_camera/src/multi_image_view.cpp
@@ -1,6 +1,26 @@
-#include "ros/ros.h"
-#include "sensor_msgs/Image.h"
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <boost/thread/mutex.hpp>
 
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/time_synchronizer.h>
+
+using namespace message_filters;
+using namespace sensor_msgs;
+typedef sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::CameraInfo> ImageSyncPolicy;
+
+void callback(const ImageConstPtr& image, const CameraInfoConstPtr& cam_info)
+{
+	return;
+}
 
 int main(int argc, char **argv)
 {
@@ -18,22 +38,24 @@ int main(int argc, char **argv)
 	// (iii) aggregate all images into one multi_view
 	// (iv)  publish the multi_view image
 
-	std::vector<std::string> vec_cameras;
-	std::vector<std::string> vec_camera_infos;
-	topics.push_back("/stereo/left/image_raw");
-	vec_camera_infos.push_back("/stereo/left/camera_info");
+	std::vector<std::string> vec_camera_info_topics;
+	std::vector<std::string> vec_camera_topics;
 	
-	topics.push_back("/stereo/right/image_raw");
-	vec_camera_infos.push_back("/stereo/right/camera_info");
+	vec_camera_topics.push_back("/stereo/left/image_raw");
+	vec_camera_info_topics.push_back("/stereo/left/camera_info");
 	
-	topics.push_back("/cam3d/rgb/image_color");
-	vec_camera_infos.push_back("/cam3d/rgb/camera_info");
-
-	typedef sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::CameraInfo> ImageSyncPolicy;
+	vec_camera_topics.push_back("/stereo/right/image_raw");
+	vec_camera_info_topics.push_back("/stereo/right/camera_info");
+	
+	vec_camera_topics.push_back("/cam3d/rgb/image_color");
+	vec_camera_info_topics.push_back("/cam3d/rgb/camera_info");
 
 	//TODO: register a variable amount of subscribers
 	std::vector<image_transport::SubscriberFilter> vec_camera_image_sub;
-	for (int i=0; i<topics.size(); i++)
+	std::vector<boost::shared_ptr<image_transport::ImageTransport> > vec_image_transport;
+	std::vector<message_filters::Subscriber<sensor_msgs::CameraInfo> > vec_camera_info;
+	std::vector<boost::shared_ptr<message_filters::Synchronizer<ImageSyncPolicy > > > vec_image_sub_sync;
+	for (unsigned int i=0; i<vec_camera_topics.size(); i++)
 	{
 		image_transport::SubscriberFilter camera_image_sub_i;
 		boost::shared_ptr<image_transport::ImageTransport> image_transport_i = 
@@ -41,17 +63,20 @@ int main(int argc, char **argv)
 		message_filters::Subscriber<sensor_msgs::CameraInfo> camera_info_sub_i;
 		boost::shared_ptr<message_filters::Synchronizer<ImageSyncPolicy > > image_sub_sync_i;
 		
-		camera_image_sub_i.subscribe(*image_transport_i, topics[i], 1);
-        camera_info_sub_i.subscribe(node_handle_, "camera_info", 1);
+		camera_image_sub_i.subscribe(*image_transport_i, vec_camera_topics[i], 1);
+        camera_info_sub_i.subscribe(nh, vec_camera_info_topics[i], 1);
 		
 		image_sub_sync_i = boost::shared_ptr<message_filters::Synchronizer<ImageSyncPolicy> >(new message_filters::Synchronizer<ImageSyncPolicy>(ImageSyncPolicy(3)));
         image_sub_sync_i->connectInput(camera_image_sub_i, camera_info_sub_i);
-        image_sub_sync_i->registerCallback(boost::bind(&???????????????, this, _1, _2));
+        image_sub_sync_i->registerCallback(boost::bind(&callback, _1, _2));
 		
-		vec_camera_image_sub_.push_back(camera_image_sub);
+		vec_camera_image_sub.push_back(camera_image_sub_i);
+		vec_image_transport.push_back(image_transport_i);
+		vec_camera_info.push_back(camera_info_sub_i);
+		vec_image_sub_sync.push_back(image_sub_sync_i);
 	}
 
-	// publish cylcically current multi_view image
+	// publish multi_view images
 	ros::Rate loop_rate(10);
 	while (ros::ok())
 	{
@@ -61,7 +86,6 @@ int main(int argc, char **argv)
 		//multi_image_pub.publish(multi_view);
 
 		ros::spinOnce();
-
 		loop_rate.sleep();
 	}
 
