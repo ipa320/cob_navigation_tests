488671a53f724d074ebcd7de3a8b66b687f5c55b
SimonEbner <Simon.Ebner@gmail.com>
2013-08-09 14:18:40 +0200

    Enhanced automation script with analysis

--- ipa_tests_sim/automation.bash
+++ ipa_tests_sim/automation.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 mkdir -p /share/uhr-se/bagrecord
 export ROBOT_ENV=ipa-apartment
 export ROS_MASTER_URI=http://localhost:22422
@@ -10,12 +10,25 @@ function killAllRosAndGazebo(){
 while true; do
     mkdir -p /share/uhr-se/bagrecord
 
-    for route in "room" "ipa-apartment"; do
-        for robot in "desire" "cob3-3"; do
-            for nav in "2dnav_ipa_extloc_diff" "2dnav_ipa_extloc" "2dnav_ros_dwa" "2dnav_ros_tr"; do
-                if ( [ $nav == "2dnav_ipa_extloc_diff" ] || [ $nav == "2dnav_ipa_extloc" ] ) && [ $robot == "desire" ]; then
+    for route in "room"; do
+        for robot in "desire" "raw3-1"; do
+            for nav in "2dnav_ros_dwa" "2dnav_ros_tr" "2dnav_ipa_extloc" "2dnav_ipa_extloc_diff"; do
+                if ( [ $nav == "2dnav_ipa_extloc_diff" ] || [ $nav == "2dnav_ipa_extloc" ] ); then
+                    if [ $robot != "cob3-3" ]; then 
+                        continue 
+                    fi
+                else
+                    if [ $robot == "cob3-3" ]; then
+                        continue
+                    fi
+                fi
+                if [ $nav == "2dnav_ros_tr" ] && [ $route == "room" ]; then
                     continue;
                 fi
+                sleep 2
+                killAllRosAndGazebo; sleep 2; killAllRosAndGazebo; sleep 10
+                timeout -s 9 2000 roslaunch navigation_test_analysis analyse_remaining_bag_files.launch keepalive:=false bagPath:=/share/uhr-se/bagrecord
+                sleep 10
 
                 echo "Starting $robot on $route with $nav"
                 sleep 2
