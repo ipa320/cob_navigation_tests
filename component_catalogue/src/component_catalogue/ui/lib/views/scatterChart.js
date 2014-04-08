ac27cba52941c71d8797de69a6b4f57220064036
Simon Ebner <Simon.Ebner@gmail.com>
2014-04-08 14:47:51 +0200

    Proper tooltip, remove debug statements

--- component_catalogue/src/component_catalogue/ui/lib/views/scatterChart.js
+++ component_catalogue/src/component_catalogue/ui/lib/views/scatterChart.js
@@ -18,7 +18,6 @@
       resetSeries: function(model, series) {
         var _ref;
 
-        console.log('okay changed');
         if (!this.chart) {
           return;
         }
@@ -27,36 +26,17 @@
         }
         this.chart.counters.color = 0;
         this.chart.counters.symbol = 0;
-        this.chart.addSeries(series, {
+        return this.chart.addSeries(series, {
           redraw: true,
           animation: false
         });
-        return console.log('series added');
       },
       render: function() {
         this.$el.html(this.chartContainer);
         return this;
       },
       tooltip: function(point) {
-        var date, index, label, options, roundedY, unit;
-
-        options = point.point.options;
-        date = options.date;
-        index = options.index;
-        roundedY = Math.round(point.y * 100) / 100;
-        label = this.options.label;
-        unit = this.options.unit;
-        return "Test #" + (+index + 1) + " of current series       <br>Date: " + (this.formatDate(date)) + "       <br>" + label + ": " + roundedY + " " + unit;
-      },
-      formatDate: function(date) {
-        var day, hour, minute, month, year;
-
-        year = date.getFullYear();
-        month = date.getMonth();
-        minute = date.getMinutes();
-        hour = date.getHours();
-        day = date.getDate();
-        return "" + day + "." + month + "." + year + " " + hour + ":" + minute;
+        return "Timestamp: " + point.x + "<br>      Value: " + point.y;
       },
       highchartsConfig: function() {
         var self;
--- component_catalogue/src/component_catalogue/ui/src/views/scatterChart.coffee
+++ component_catalogue/src/component_catalogue/ui/src/views/scatterChart.coffee
@@ -12,36 +12,19 @@ define [ 'backbone', 'highcharts' ], ( Backbone, Highcharts )->
         @chart = chart
 
     resetSeries: ( model, series )->
-      console.log 'okay changed'
       return if not @chart
       do @chart.series[ 0 ]?.remove
       @chart.counters.color  = 0
       @chart.counters.symbol = 0
       @chart.addSeries series, redraw: true, animation: false
-      console.log 'series added'
 
     render: ->
       @$el.html @chartContainer
       this
 
     tooltip: ( point )->
-      options  = point.point.options
-      date     = options.date
-      index    = options.index
-      roundedY = Math.round( point.y*100 )/100
-      label    = @options.label
-      unit     = @options.unit
-      "Test ##{+index+1} of current series
-       <br>Date: #{@formatDate date}
-       <br>#{label}: #{roundedY} #{unit}"
-
-    formatDate: ( date )->
-      year    = date.getFullYear()
-      month   = date.getMonth()
-      minute  = date.getMinutes()
-      hour    = date.getHours()
-      day     = date.getDate()
-      "#{day}.#{month}.#{year} #{hour}:#{minute}"
+      "Timestamp: #{point.x}<br>
+      Value: #{point.y}"
 
     highchartsConfig: ->
       self = @
