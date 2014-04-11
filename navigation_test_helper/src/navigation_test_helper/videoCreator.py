59d63d8d8fc39e679cd6119210e7ae35ab88697c
ct2034 <post@henkelchristian.de>
2014-04-11 12:11:44 +0200

    adding colons

--- navigation_test_helper/src/navigation_test_helper/videoCreator.py
+++ navigation_test_helper/src/navigation_test_helper/videoCreator.py
@@ -10,35 +10,35 @@ import commands
     
 class VideoCreator( object ):
     def __init__( self, videofilepath ):
-        print '>> videoCreator init' 
+        print '>> videoCreator: init' 
         self._videofilepath = videofilepath
-        print '>> videoCreator path: ' + self._videofilepath
+        print '>> videoCreator: path: ' + self._videofilepath
         self.r = rospy.Rate(1) #Hz
         self.timeout = 10 #s     
         self.waittime = self.timeout  
         
     def createVideo( self, filename ):
         #filename = 'dateiname'
-        print '>> videoCreator CREATING'
+        print '>> videoCreator: CREATING'
         command = 'cd %s; ' % self._videofilepath
         command = command + 'avconv -r 1 -i frame%06d.jpg'
         command = command + ' -r 20 %s.mp4' % filename
         try:
             s=commands.getstatusoutput(command)
         except:
-            print ">> videoCreator Unexpected error: ", sys.exc_info()[0]
+            print ">> videoCreator: Unexpected error: ", sys.exc_info()[0]
         print s[1]   
         print s[0]   
 
     def hasFrameFiles( self ):
         s=commands.getstatusoutput('cd %s; ls | grep "frame"' % self._videofilepath)
         if s[1] == '':
-            print '>> videoCreator videofolder empty (no frames*.jpg files)'
+            print '>> videoCreator: videofolder empty (no frames*.jpg files)'
             return 0
         else:
             return 1
             
     def deleteFrameFiles( self ):
         s=commands.getstatusoutput('cd %s; rm frame*.jpg' % self._videofilepath)
-        print '>> videoCreator deleting frames'
+        print '>> videoCreator: deleting frames'
         return s[0]
